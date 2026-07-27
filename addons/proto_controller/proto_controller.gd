87027e15d76d421fa69b96a2c951f221f63c4aaf
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-27 15:39:36 -0400

    f

--- addons/proto_controller/proto_controller.gd
+++ addons/proto_controller/proto_controller.gd
@@ -4,18 +4,19 @@ extends CharacterBody2D
 @export var jump_velocity = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var gravity := 980
+var gravDir := 1
 func _physics_process(delta):
 	if !is_multiplayer_authority():
 		return
+		
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if (not is_on_floor() and gravDir == 1) or (not is_on_ceiling() and gravDir == -1):
+		velocity.y += gravity * gravDir * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("ui_accept") and ((is_on_floor() and gravDir == 1) or (is_on_ceiling() and gravDir == -1)):
+		velocity.y = jump_velocity * gravDir
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- scripts/level1.gd
+++ scripts/level1.gd
@@ -8,19 +8,25 @@ var roles = [
 	"platform"
 ]
 var ability: String
-
+var activePlatform = 1
 @onready var game: Node2D = get_parent()
 @onready var cam: Camera2D = get_parent().get_node("cam")
+@onready var player: CharacterBody2D = get_parent().get_node("players").get_node(str(multiplayer.get_unique_id()))
 
 func _ready() -> void:
 	roles.shuffle()
 	for index in game.ids.size():
 		assign_ability.rpc_id(game.ids[index],roles[index])
-
+	
+	if activePlatform == 1:
+		for i in $"spawnable platforms1".get_children():
+			i.get_child(0).disabled = true
+			i.get_child(1).modulate = Color("333333ff")
 @rpc("any_peer","call_local","reliable")
 func assign_ability(assignedAbility:String) -> void:
-	print(ability)
+	
 	ability = assignedAbility
+	ability = "platform"
 	
 func _physics_process(delta: float) -> void:
 	if !multiplayer.is_server():
@@ -32,9 +38,45 @@ func _physics_process(delta: float) -> void:
 	cam.position = cam.position.lerp(targetCam, 5 * delta)
 	$screenWalls.position = cam.position
 	
+	if Input.is_action_just_pressed("ability"):
+		if ability == "gravity":
+			switch_grav.rpc()
+			
+		if ability == "size":
+			change_size.rpc()
+			
+		if ability == "platform":
+			change_platforms.rpc()
+			
+@rpc("any_peer","call_local","reliable")
+func switch_grav() -> void:
+	player.gravDir *= -1
 	
+@rpc("any_peer","call_local","reliable")
+func change_size() -> void:
+	if player.scale == Vector2(1.0,1.0):
+		player.scale = Vector2(2.0,2.0)
+	elif player.scale == Vector2(2.0,2.0):
+		player.scale = Vector2(0.5,0.5)
+	elif player.scale == Vector2(0.5,0.5):
+		player.scale = Vector2(1.0,1.0)
 	
-	
-	
-	
-	
+@rpc("any_peer","call_local","reliable")
+func change_platforms() -> void:
+	activePlatform *= -1
+	if activePlatform == 1:
+		for i in $"spawnable platforms1".get_children():
+			i.get_child(0).disabled = true
+			i.get_child(1).modulate = Color("333333ff")
+			
+		for i in $"spawnable platforms2".get_children():
+			i.get_child(0).disabled = false
+			i.get_child(1).modulate = Color("ffffffff")
+		
+	else:
+		for i in $"spawnable platforms2".get_children():
+			i.get_child(0).disabled = true
+			i.get_child(1).modulate = Color("333333ff")
+		for i in $"spawnable platforms1".get_children():
+			i.get_child(0).disabled = false
+			i.get_child(1).modulate = Color("ffffffff")
