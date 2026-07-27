78166547165d51f34e79d274e489d77546047365
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-26 22:07:12 -0400

    f

---
+++ scripts/node_2d.gd
@@ -0,0 +1,78 @@
+extends Node2D
+
+
+
+const PROTO_CONTROLLER = preload("uid://bs72ogkvdd7d6")
+
+
+var players: Array[CharacterBody2D]
+var spawnOrder: int
+var currentSpawn: int = 0
+
+
+
+
+
+var ids = []
+
+func _ready():
+	Networking.host_created.connect(_on_host_created)
+	multiplayer.peer_connected.connect(_peer_connected)
+	
+	
+	
+	
+func _on_host_created():
+	spawnOrder = currentSpawn
+	currentSpawn += 1
+	
+func _peer_connected(peer_id:int):
+	ids.append(peer_id)
+	if multiplayer.is_server():
+		get_spawn_order(currentSpawn)
+		currentSpawn += 1
+@rpc("any_peer","reliable")
+func get_spawn_order(num) -> void:
+	spawnOrder = num
+
+func _on_button_pressed() -> void:
+	$CanvasLayer/host.disabled = true
+	Networking.host_lobby()
+
+
+func _on_start_pressed() -> void:
+	if !multiplayer.is_server():
+		return
+	spawn_player.rpc(multiplayer.get_unique_id())
+	for id in ids:
+		spawn_player.rpc(id)
+	hide_buttons.rpc()
+	
+	
+@rpc("any_peer","call_local","reliable")
+func hide_buttons() -> void:
+	$CanvasLayer/host.hide()
+	$CanvasLayer/start.hide()
+	
+	
+func initialize_player(player:CharacterBody2D):
+	player.global_position = $spawnpoint.position
+
+	for other in players:
+		player.add_collision_exception_with(other)
+		other.add_collision_exception_with(player)
+
+	players.append(player)
+	
+@rpc("authority","call_local","reliable")
+func spawn_player(peer_id:int):
+	$CanvasLayer/waiting.hide()
+	var player := PROTO_CONTROLLER.instantiate()
+	player.name = str(peer_id)
+	player.set_multiplayer_authority(peer_id)
+	if peer_id == multiplayer.get_unique_id():
+		player.get_node("Camera2D").make_current()
+	$players.add_child(player)
+	initialize_player(player)
+	
+	
